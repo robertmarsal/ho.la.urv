a8050c53b40f606fe0cf5d0284e6eca3620d9891
Robert Boloc <robertboloc@gmail.com>
2013-06-01 23:03:56 +0200

    obtaining basic info and displaying it

--- app/src/com/robertboloc/eu/holaurv/DisplayActivity.java
+++ app/src/com/robertboloc/eu/holaurv/DisplayActivity.java
@@ -9,6 +9,9 @@ import com.robertboloc.eu.holaurv.lib.Evalos;
 public class DisplayActivity extends Activity {
 
 	private TextView mName;
+	private TextView mShift;
+	private TextView mUsername;
+	private TextView mCode;
 	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
@@ -16,6 +19,9 @@ public class DisplayActivity extends Activity {
 		setContentView(R.layout.activity_display);
 		
 		mName = (TextView)findViewById(R.id.name);
+		mShift = (TextView)findViewById(R.id.shift);
+		mCode = (TextView)findViewById(R.id.code);
+		mUsername = (TextView)findViewById(R.id.username);
 		
 		refresh();
 	}
@@ -24,9 +30,18 @@ public class DisplayActivity extends Activity {
 		// Obtain the application state
 		HoLaURV appState = ((HoLaURV)getApplicationContext());
 		Evalos eva = appState.getEva();
-		
+
 		// Update the name
 		mName.setText(eva.getName());
+
+		// Update the shift
+		mShift.setText(eva.getShift());
+
+		// Update the username
+		mUsername.setText(eva.getUsername());
+
+		// Update the code
+		mCode.setText(eva.getCode());
 	}
 
 }
--- app/src/com/robertboloc/eu/holaurv/lib/Evalos.java
+++ app/src/com/robertboloc/eu/holaurv/lib/Evalos.java
@@ -1,8 +1,13 @@
 package com.robertboloc.eu.holaurv.lib;
 
+import java.util.List;
+
+import org.joda.time.DateTime;
+
 import com.gistlabs.mechanize.MechanizeAgent;
 import com.gistlabs.mechanize.document.Document;
 import com.gistlabs.mechanize.document.html.HtmlDocument;
+import com.gistlabs.mechanize.document.html.HtmlElement;
 import com.gistlabs.mechanize.document.html.HtmlElements;
 import com.gistlabs.mechanize.document.html.form.Form;
 import com.gistlabs.mechanize.document.html.query.HtmlQueryBuilder;
@@ -12,6 +17,7 @@ public class Evalos {
 	private MechanizeAgent agent;
 	private HtmlDocument response;
 	private HtmlElements htmlElements;
+	private List<HtmlElement> weekActivity;
 
 	private final String webAppBaseUrl = "http://gestiodelapresencia.urv.cat/evalos/login.html";
 
@@ -43,7 +49,80 @@ public class Evalos {
 		return this;
 	}
 
+	/**
+	 * Obtains the username.
+	 * 
+	 * @return String
+	 */
+	public String getUsername() {
+		return this.username;
+	}
+
+	/**
+	 * Obtains the full name of the user.
+	 * 
+	 * @return String
+	 */
 	public String getName() {
-		return htmlElements.get(HtmlQueryBuilder.byWidth("306").and.byHeight("19")).getText().replace("Nom:", "");
+		return htmlElements.get(HtmlQueryBuilder.byWidth("306").and.
+												 byHeight("19").and.
+												 byClass("verdana10bold")).getText().replace("Nom:", "")
+												 								    .replace("\n", "")
+												 								    .replace("\r", "")
+												 									.trim();
+	}
+
+	/**
+	 * Obtains the internal code assigned to the user.
+	 * 
+	 * @return String
+	 */
+	public String getCode() {
+		return htmlElements.get(HtmlQueryBuilder.byWidth("143").and.
+											     byHeight("19").and.
+											     byClass("verdana10bold")).getText().replace("Codi:", "");
+	}
+
+	/**
+	 * Obtains todays' shift.
+	 * 
+	 * @return String
+	 */
+	public String getShift() {
+		HtmlElement today = getWeekActivity().get(getCustomIntDayOfTheWeek());
+		return today.get(HtmlQueryBuilder.byClass("verdana12").and.
+										  byWidth("75").and.
+									      byHeight("40")).get(HtmlQueryBuilder.byTag("div")).getInnerHtml();
+	}
+
+	/**
+	 * Returns the current day of the week shifted with one position.
+	 * 
+	 * @return int
+	 */
+	public int getCustomIntDayOfTheWeek()
+	{
+		DateTime date = org.joda.time.DateTime.now();
+		return date.getDayOfWeek() - 1;
+	}
+
+	/**
+	 * Returns the week activity.
+	 * 
+	 * @return List<HtmlElement>
+	 */
+	private List<HtmlElement> getWeekActivity() {
+		if(weekActivity != null) {
+			return weekActivity;
+		} else {
+
+		weekActivity = htmlElements.getAll(HtmlQueryBuilder.byTag("table").and.
+												   byWidth("680").and.
+												   byHeight("41").and.
+												   by("cellspacing", "0").and.
+												   by("cellpadding", "0"));
+		}
+
+		return weekActivity;
 	}
 }
