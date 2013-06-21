324ef1037b396eecdf77e6ea2962058bd2a74cac
Robert Boloc <robertboloc@gmail.com>
2013-06-22 01:40:57 +0200

    set the current day as default on pager

--- app/src/com/robertboloc/eu/holaurv/DisplayActivity.java
+++ app/src/com/robertboloc/eu/holaurv/DisplayActivity.java
@@ -1,5 +1,7 @@
 package com.robertboloc.eu.holaurv;
 
+import org.joda.time.DateTime;
+
 import android.os.Bundle;
 import android.support.v4.app.FragmentActivity;
 import android.support.v4.view.ViewPager;
@@ -14,10 +16,13 @@ public class DisplayActivity extends FragmentActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_display);
 
+        DateTime date = org.joda.time.DateTime.now();
+
         mDayCollectionPagerAdapter = new DayCollectionPagerAdapter(
                 getSupportFragmentManager(), this);
 
         mViewPager = (ViewPager) findViewById(R.id.pager);
         mViewPager.setAdapter(mDayCollectionPagerAdapter);
+        mViewPager.setCurrentItem(date.getDayOfWeek());
     }
 }
