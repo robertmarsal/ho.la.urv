fc974ed7fc031d598b200773d8c04f246cfd6623
Robert Boloc <robertboloc@gmail.com>
2013-06-28 23:10:14 +0200

    added action bar (using action bar cherlock)

--- app/src/com/robertboloc/eu/holaurv/DisplayActivity.java
+++ app/src/com/robertboloc/eu/holaurv/DisplayActivity.java
@@ -3,13 +3,19 @@ package com.robertboloc.eu.holaurv;
 import org.joda.time.DateTime;
 
 import android.os.Bundle;
-import android.support.v4.app.FragmentActivity;
+import android.support.v4.app.FragmentTransaction;
 import android.support.v4.view.ViewPager;
 
-public class DisplayActivity extends FragmentActivity {
+import com.actionbarsherlock.app.ActionBar;
+import com.actionbarsherlock.app.ActionBar.Tab;
+import com.actionbarsherlock.app.SherlockFragmentActivity;
+
+public class DisplayActivity extends SherlockFragmentActivity implements
+        com.actionbarsherlock.app.ActionBar.TabListener {
 
     ViewPager mViewPager;
     DayCollectionPagerAdapter mDayCollectionPagerAdapter;
+    ActionBar actionBar;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -24,5 +30,36 @@ public class DisplayActivity extends FragmentActivity {
         mViewPager = (ViewPager) findViewById(R.id.pager);
         mViewPager.setAdapter(mDayCollectionPagerAdapter);
         mViewPager.setCurrentItem(date.getDayOfWeek());
+
+        actionBar = getSupportActionBar();
+        actionBar.setNavigationMode(ActionBar.NAVIGATION_MODE_TABS);
+
+        mViewPager
+                .setOnPageChangeListener(new ViewPager.SimpleOnPageChangeListener() {
+                    @Override
+                    public void onPageSelected(int position) {
+                        actionBar.setSelectedNavigationItem(position);
+                    }
+                });
+
+        for (int i = 0; i < mDayCollectionPagerAdapter.getCount(); i++) {
+            actionBar.addTab(actionBar.newTab()
+                    .setText(mDayCollectionPagerAdapter.getPageTitle(i))
+                    .setTabListener(this));
+        }
     }
+
+    @Override
+    public void onTabSelected(Tab tab, FragmentTransaction ft) {
+        mViewPager.setCurrentItem(tab.getPosition());
+    }
+
+    @Override
+    public void onTabUnselected(Tab tab, FragmentTransaction ft) {
+    }
+
+    @Override
+    public void onTabReselected(Tab tab, FragmentTransaction ft) {
+    }
+
 }
